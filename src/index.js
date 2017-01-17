2682b5b299e6cb4b7802ba14fef981ee6fcdb662
Andrzej Zaleski <azaleski23@gmail.com>
2017-01-17 19:52:51 +0100

    v17: Redux: The Middleware Chain

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 
 import configureStore from './configureStore';
 import Root from './components/Root'
-import { fetchTodos } from './api'
-
-fetchTodos('all').then(todos =>
-  console.log(todos)
-);
 
 const store = configureStore();
 

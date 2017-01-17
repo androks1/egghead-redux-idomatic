0c083bef3b5dc45a6b15bfebf225a2e4af1d6306
Andrzej Zaleski <azaleski23@gmail.com>
2017-01-17 18:30:09 +0100

    v10: Colocating Selectors with reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,40 +1,5 @@
 import { combineReducers } from 'redux';
-
-const todo = (state, action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false
-      };
-    case 'TOGGLE_TODO':
-      if (state.id !== action.id) {
-        return state;
-      }
-
-      return {
-        ...state,
-        completed: !state.completed
-      };
-    default:
-      return state;
-  }
-}
-
-const todos = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action)
-      ];
-    case 'TOGGLE_TODO':
-      return state.map(todoItem => todo(todoItem, action));
-    default:
-      return state;
-  }
-};
+import todos, * as fromTodos from './todos';
 
 const visibilityFilter = (
     state = 'all',
@@ -54,3 +19,6 @@ const todoApp = combineReducers({
 });
 
 export default todoApp;
+
+export const getVisibleTodos = (state, filter) =>
+  fromTodos.getVisibleTodos(state.todos, filter);
